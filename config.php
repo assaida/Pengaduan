fdec224f156918cf26f6f78f5bca4846e190ad40
qilaa1 <cahyaaqiilah@gmail.com>
2026-01-09 16:36:47 +0700

    modified

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php 
-// $con = mysqli_connect('localhost', 'u1522649_halim1', 'Polgan12345!!','u1522649_halim');
 $con = mysqli_connect('localhost', 'root', '','pelayanan_pln');
 
 if(mysqli_connect_errno()){
